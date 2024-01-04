bd8a2766867a785ccbd0983c18e1c9ab5923438a
sskowalik <skowalik@student.agh.edu.pl>
2024-01-05 00:17:26 +0100

    robie commita

--- react_files/app/index.js
+++ react_files/app/index.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from './style-login';
+
 const Login = () => {
-    return (
-        <View style={styles.container}>
-                <View style={styles.maskGroup}>
-                <View style={styles.rectangle13}></View>
-                <View style={styles.orzelBialy3}></View>
-                <View style={styles.rectangle14}></View>
-                <View style={styles.rectangle15}></View>
-                <View style={styles.line3}></View>
-                <View style={styles.info2}></View>
-                <View style={styles.coi2}></View>
-                <View style={styles.znakUproszczony2}></View>
-                <Text style={styles.wersja01}>wersja 0.1</Text>
-                <Text style={styles.urzedasPl}>URZĘDAS.PL</Text>
-                <View style={styles.herbPolski2}></View>
-                <View style={styles.image1}></View>
-                <View style={styles.rectangle22}></View>
-                <View style={styles.rectangle23}></View>
-                <View style={styles.rectangle25}></View>
-                <View style={styles.rectangle24}></View>
-                <Text style={styles.logowanie}>LOGOWANIE</Text>
-                <Text style={styles.email}>E-mail</Text>
-                <Text style={styles.haslo}>Hasło</Text>
-                <Text style={styles.noAccount}>Nie posiadasz konta?</Text>
-                <Text style={styles.forgotPassword}>Nie pamiętasz hasła?</Text>
-                <Text style={styles.register}>Zarejestruj się</Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.maskGroup}>
+      <View style={styles.rectangle13}></View>
+      <Image style={styles.orzelBialy3} source={require('react_files\app\images\orzel_bialy.png')} />
+      <View style={styles.rectangle14}></View>
+      <View style={styles.rectangle15}></View>
+      <View style={styles.line3}></View>
+      <Image style={styles.info2} source={require('react_files\app\images\info.png')} />
+      <Image style={styles.coi2} source={require('react_files\app\images\COI.png')} />
+      <Image style={styles.znakUproszczony2} source={require('react_files\app\images\ministerstwo_cyfryzacji.png')} />
+      <Text style={styles.wersja01}>wersja 0.1</Text>
+      <Text style={styles.urzedasPl}>URZĘDAS.PL</Text>
+      <Image style={styles.herbPolski2} source={require('react_files\app\images\herb_Polski.png')} />
+      <Image style={styles.image1} source={require('react_files\app\images\logo.png')} />
+      <View style={styles.rectangle22}></View>
+      <View style={styles.rectangle23}></View>
+      <View style={styles.rectangle25}></View>
+      <View style={styles.rectangle24}></View>
+      <Text style={styles.logowanie}>LOGOWANIE</Text>
+      <Text style={styles.email}>E-mail</Text>
+      <Text style={styles.haslo}>Hasło</Text>
+      <Text style={styles.noAccount}>Nie posiadasz konta?</Text>
+      <Text style={styles.forgotPassword}>Nie pamiętasz hasła?</Text>
+      <Text style={styles.register}>Zarejestruj się</Text>
+    </View>
+  );
 };
+
 export default Login;
--- react_files/app/style-login.js
+++ react_files/app/style-login.js
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-   /* Mask group */
-   maskGroup: {
+  /* Mask group */
+  maskGroup: {
     position: 'absolute',
     width: '40%',
     height: '60%',
@@ -11,12 +11,11 @@ export const styles = StyleSheet.create({
   /* Rectangle 13 */
   rectangle13: {
     position: 'absolute',
-    left: '0%',
+    left: 0,
     right: '4.64%',
-    top: '0%',
-    bottom: '0%',
-    background: '#FFFFFF',
-    mixBlendMode: 'normal',
+    top: 0,
+    bottom: 0,
+    backgroundColor: '#FFFFFF',
   },
 
   /* orzel_bialy 3 */
@@ -26,37 +25,38 @@ export const styles = StyleSheet.create({
     right: '4.64%',
     top: '48.75%',
     bottom: '9.61%',
-    background: { uri: 'react_files\app\images\orzel_bialy.png' },
+    resizeMode: 'cover', // Dodane dla obrazu
   },
 
   /* Rectangle 14 */
   rectangle14: {
     position: 'absolute',
-    left: '0%',
+    left: 0,
     right: '4.64%',
     top: '90.39%',
-    bottom: '0%',
-    background: '#CB3D49',
+    bottom: 0,
+    backgroundColor: '#CB3D49',
   },
 
   /* Rectangle 15 */
   rectangle15: {
     position: 'absolute',
-    left: '0%',
+    left: 0,
     right: '4.64%',
-    top: '0%',
+    top: 0,
     bottom: '92.97%',
-    background: '#F2F2F2',
+    backgroundColor: '#F2F2F2',
   },
 
   /* Line 3 */
   line3: {
     position: 'absolute',
-    left: '0%',
+    left: 0,
     right: '4.64%',
     top: '7.03%',
     bottom: '92.97%',
-    border: '1px solid #B3B3B3',
+    borderWidth: 1,
+    borderColor: '#B3B3B3',
   },
 
   /* info 2 */
@@ -66,8 +66,7 @@ export const styles = StyleSheet.create({
     right: '5.3%',
     top: '0.39%',
     bottom: '93.28%',
-    background: { uri: 'react_files\app\images\info.png' },
-    backgroundSize: 'cover',
+    resizeMode: 'cover', // Dodane dla obrazu
   },
 
   /* COI 2 */
@@ -77,8 +76,7 @@ export const styles = StyleSheet.create({
     right: '72.45%',
     top: '92.42%',
     bottom: '1.95%',
-    background: { uri: 'react_files\app\images\COI.png' },
-    backgroundSize: 'cover',
+    resizeMode: 'cover', // Dodane dla obrazu
   },
 
   /* 05_znak_uproszczony_kolor_biale_tlo 2 */
@@ -88,15 +86,14 @@ export const styles = StyleSheet.create({
     right: '47.15%',
     top: '92.03%',
     bottom: '1.64%',
-    background: { uri: 'react_files\app\images\ministerstwo_cyfryzacji.png' },
-    backgroundSize: 'cover',
+    resizeMode: 'cover', // Dodane dla obrazu
   },
 
   /* wersja 0.1 */
   wersja01: {
     position: 'absolute',
     left: '73.11%',
-    right: '0%',
+    right: 0,
     top: '95.94%',
     bottom: '0.94%',
     fontStyle: 'normal',
@@ -127,8 +124,7 @@ export const styles = StyleSheet.create({
     right: '63.71%',
     top: '1.25%',
     bottom: '94.22%',
-    background: { uri: 'react_files\app\images\herb_Polski.png' },
-    backgroundSize: 'cover',
+    resizeMode: 'cover', // Dodane dla obrazu
   },
 
   /* image 1 */
@@ -139,9 +135,9 @@ export const styles = StyleSheet.create({
     right: '36.16%',
     top: '10.47%',
     bottom: '70.39%',
-    background: { uri: 'react_files\app\images\logo.png' },
-    backgroundSize: 'cover',
-    border: '4px solid #000000',
+    resizeMode: 'cover', // Dodane dla obrazu
+    borderWidth: 4,
+    borderColor: '#000000',
   },
 
   /* Rectangle 22 */
@@ -152,127 +148,130 @@ export const styles = StyleSheet.create({
     right: '16.03%',
     top: '34.84%',
     bottom: '43.52%',
-    border: '3px solid #D6D6D6',
+    borderWidth: 3,
+    borderColor: '#D6D6D6',
     borderRadius: 40,
   },
 
   rectangle23: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      left: '15.5%',
-      right: '21.99%',
-      top: '38.83%',
-      bottom: '56.8%',
-      border: '3px solid #D6D6D6',
-      borderRadius: 40,
-    },
-  
-    /* Rectangle 25 */
-    rectangle25: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      left: '15.5%',
-      right: '21.99%',
-      top: '48.12%',
-      bottom: '47.5%',
-      border: '3px solid #D6D6D6',
-      borderRadius: 40,
-    },
-  
-    /* Rectangle 24 */
-    rectangle24: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      left: '15.5%',
-      right: '21.99%',
-      top: '38.83%',
-      bottom: '56.8%',
-      border: '3px solid #D6D6D6',
-      borderRadius: 40,
-    },
-  
-    /* LOGOWANIE */
-    logowanie: {
-      position: 'absolute',
-      left: '31.52%',
-      right: '36.03%',
-      top: '31.09%',
-      bottom: '65.16%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 40,
-      lineHeight: 48,
-      color: '#B3B3B3',
-    },
-  
-    /* E-mail */
-    email: {
-      position: 'absolute',
-      left: '18.81%',
-      right: '71.39%',
-      top: '36.56%',
-      bottom: '61.17%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 24,
-      lineHeight: 29,
-      color: '#B3B3B3',
-    },
-  
-    /* Hasło */
-    haslo: {
-      position: 'absolute',
-      left: '18.81%',
-      right: '72.45%',
-      top: '45.86%',
-      bottom: '51.88%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 24,
-      lineHeight: 29,
-      color: '#B3B3B3',
-    },
-  
-    /* Nie posiadasz konta? */
-    noAccount: {
-      position: 'absolute',
-      left: '15.5%',
-      right: '51.79%',
-      top: '56.48%',
-      bottom: '41.25%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 24,
-      lineHeight: 29,
-      color: '#B3B3B3',
-    },
-  
-    /* Nie pamiętam hasła */
-    forgotPassword: {
-      position: 'absolute',
-      left: '45.7%',
-      right: '24.11%',
-      top: '52.5%',
-      bottom: '45.23%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 24,
-      lineHeight: 29,
-      color: '#656565',
-    },
-  
-    /* Zarejestruj się */
-    register: {
-      position: 'absolute',
-      left: '15.5%',
-      right: '62.52%',
-      top: '58.75%',
-      bottom: '38.98%',
-      fontStyle: 'normal',
-      fontWeight: '600',
-      fontSize: 24,
-      lineHeight: 29,
-      color: '#656565',
-    },
+    boxSizing: 'border-box',
+    position: 'absolute',
+    left: '15.5%',
+    right: '21.99%',
+    top: '38.83%',
+    bottom: '56.8%',
+    borderWidth: 3,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+  },
 
+  /* Rectangle 25 */
+  rectangle25: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    left: '15.5%',
+    right: '21.99%',
+    top: '48.12%',
+    bottom: '47.5%',
+    borderWidth: 3,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+  },
+
+  /* Rectangle 24 */
+  rectangle24: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    left: '15.5%',
+    right: '21.99%',
+    top: '38.83%',
+    bottom: '56.8%',
+    borderWidth: 3,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+  },
+
+  /* LOGOWANIE */
+  logowanie: {
+    position: 'absolute',
+    left: '31.52%',
+    right: '36.03%',
+    top: '31.09%',
+    bottom: '65.16%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 40,
+    lineHeight: 48,
+    color: '#B3B3B3',
+  },
+
+  /* E-mail */
+  email: {
+    position: 'absolute',
+    left: '18.81%',
+    right: '71.39%',
+    top: '36.56%',
+    bottom: '61.17%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#B3B3B3',
+  },
+
+  /* Hasło */
+  haslo: {
+    position: 'absolute',
+    left: '18.81%',
+    right: '72.45%',
+    top: '45.86%',
+    bottom: '51.88%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#B3B3B3',
+  },
+
+  /* Nie posiadasz konta? */
+  noAccount: {
+    position: 'absolute',
+    left: '15.5%',
+    right: '51.79%',
+    top: '56.48%',
+    bottom: '41.25%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#B3B3B3',
+  },
+
+  /* Nie pamiętam hasła */
+  forgotPassword: {
+    position: 'absolute',
+    left: '45.7%',
+    right: '24.11%',
+    top: '52.5%',
+    bottom: '45.23%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#656565',
+  },
+
+  /* Zarejestruj się */
+  register: {
+    position: 'absolute',
+    left: '15.5%',
+    right: '62.52%',
+    top: '58.75%',
+    bottom: '38.98%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 24,
+    lineHeight: 29,
+    color: '#656565',
+  },
 });
