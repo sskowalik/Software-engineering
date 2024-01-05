961e3523c28a00179d61923ccef2258d65efd57c
Milosz180 <milogro@student.agh.edu.pl>
2024-01-05 01:29:13 +0100

    test commit 0.1

--- react_files/app/style-login.js
+++ react_files/app/style-login.js
@@ -1,276 +1,261 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  /* Mask group */
-  maskGroup: {
-    position: 'absolute',
-    width: '40%',
-    height: '60%',
-  },
 
-  /* Rectangle 13 */
-  rectangle13: {
-    position: 'absolute',
-    left: 0,
-    right: '4.64%',
-    top: 0,
-    bottom: 0,
-    backgroundColor: '#FFFFFF',
-  },
+container: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+},
+
+//   /* Rectangle 13 */
+//   rectangle13: {
+//     left: 0,
+//     right: '4.64%',
+//     top: 0,
+//     bottom: 0,
+//     backgroundColor: '#FFFFFF',
+//   },
 
   /* orzel_bialy 3 */
   orzelBialy3: {
-    position: 'absolute',
     left: '10.46%',
     right: '4.64%',
     top: '48.75%',
     bottom: '9.61%',
-    resizeMode: 'cover', // Dodane dla obrazu
+    // resizeMode: 'cover', // Dodane dla obrazu
+    width: '10%' ,
+    height: '10%' ,
   },
 
-  /* Rectangle 14 */
-  rectangle14: {
-    position: 'absolute',
-    left: 0,
-    right: '4.64%',
-    top: '90.39%',
-    bottom: 0,
-    backgroundColor: '#CB3D49',
-  },
+  // /* Rectangle 14 */
+  // rectangle14: {
+  //   left: 0,
+  //   right: '4.64%',
+  //   top: '90.39%',
+  //   bottom: 0,
+  //   backgroundColor: '#CB3D49',
+  // },
 
-  /* Rectangle 15 */
-  rectangle15: {
-    position: 'absolute',
-    left: 0,
-    right: '4.64%',
-    top: 0,
-    bottom: '92.97%',
-    backgroundColor: '#F2F2F2',
-  },
+  // /* Rectangle 15 */
+  // rectangle15: {
+  //   // left: 0,
+  //   // right: '4.64%',
+  //   // top: 0,
+  //   // bottom: '92.97%',
+  //   backgroundColor: '#F2F2F2',
+  //   height: '3%',
+  //   marginBottom: '202%',
+  // },
 
-  /* Line 3 */
-  line3: {
-    position: 'absolute',
-    left: 0,
-    right: '4.64%',
-    top: '7.03%',
-    bottom: '92.97%',
-    borderWidth: 1,
-    borderColor: '#B3B3B3',
-  },
+  // /* Line 3 */
+  // line3: {
+  //   left: 0,
+  //   right: '4.64%',
+  //   top: '7.03%',
+  //   bottom: '92.97%',
+  //   borderWidth: 1,
+  //   borderColor: '#B3B3B3',
+  // },
 
-  /* info 2 */
+  /* info */
   info2: {
-    position: 'absolute',
-    left: '83.97%',
-    right: '5.3%',
-    top: '0.39%',
-    bottom: '93.28%',
-    resizeMode: 'cover', // Dodane dla obrazu
+    marginLeft: '50%',
+    marginTop: '4%',
+    width: 50,
+    height: 50,
+    padding: 0,
   },
 
-  /* COI 2 */
-  coi2: {
-    position: 'absolute',
-    left: '3.31%',
-    right: '72.45%',
-    top: '92.42%',
-    bottom: '1.95%',
-    resizeMode: 'cover', // Dodane dla obrazu
-  },
+  // /* COI 2 */
+  // coi2: {
+  //   left: '3.31%',
+  //   right: '72.45%',
+  //   top: '92.42%',
+  //   bottom: '1.95%',
+  //   // resizeMode: 'cover', // Dodane dla obrazu
+  //   width: '10%' ,
+  //   height: '10%' ,
+  // },
 
-  /* 05_znak_uproszczony_kolor_biale_tlo 2 */
-  znakUproszczony2: {
-    position: 'absolute',
-    left: '28.74%',
-    right: '47.15%',
-    top: '92.03%',
-    bottom: '1.64%',
-    resizeMode: 'cover', // Dodane dla obrazu
-  },
+  // /* 05_znak_uproszczony_kolor_biale_tlo 2 */
+  // znakUproszczony2: {
+  //   left: '28.74%',
+  //   right: '47.15%',
+  //   top: '92.03%',
+  //   bottom: '1.64%',
+  //   // resizeMode: 'cover', // Dodane dla obrazu
+  // },
 
-  /* wersja 0.1 */
-  wersja01: {
-    position: 'absolute',
-    left: '73.11%',
-    right: 0,
-    top: '95.94%',
-    bottom: '0.94%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 32,
-    lineHeight: 39,
-    color: '#D6D6D6',
-  },
+  // /* wersja 0.1 */
+  // wersja01: {
+  //   left: '73.11%',
+  //   right: 0,
+  //   top: '95.94%',
+  //   bottom: '0.94%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 32,
+  //   lineHeight: 39,
+  //   color: '#D6D6D6',
+  // },
 
-  /* URZĘDAS.PL */
-  urzedasPl: {
-    position: 'absolute',
-    left: '38.15%',
-    right: '28.74%',
-    top: '1.41%',
-    bottom: '94.45%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 40,
-    lineHeight: 48,
-    color: '#000000',
-  },
+  // /* URZĘDAS.PL */
+  // urzedasPl: {
+  //   left: '38.15%',
+  //   right: '28.74%',
+  //   top: '1.41%',
+  //   bottom: '94.45%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 40,
+  //   lineHeight: 48,
+  //   color: '#000000',
+  // },
 
   /* Herb_Polski 2 */
   herbPolski2: {
-    position: 'absolute',
-    left: '29.8%',
-    right: '63.71%',
-    top: '1.25%',
-    bottom: '94.22%',
-    resizeMode: 'cover', // Dodane dla obrazu
+    width: 50,
+    height: 50,
+    marginLeft: '16%',
+    marginTop: '4%',
   },
 
-  /* image 1 */
-  image1: {
-    position: 'absolute',
-    left: '31.52%',
-    right: '36.16%',
-    top: '10.47%',
-    bottom: '70.39%',
-    resizeMode: 'cover', // Dodane dla obrazu
-    borderWidth: 4,
-    borderColor: '#000000',
-  },
+  // /* image 1 */
+  // image1: {
+  //   left: '31.52%',
+  //   right: '36.16%',
+  //   top: '10.47%',
+  //   bottom: '70.39%',
+  //   // resizeMode: 'cover', // Dodane dla obrazu
+  //   borderWidth: 4,
+  //   borderColor: '#000000',
+  //   width: '10%' ,
+  //   height: '10%' ,
+  // },
 
-  /* Rectangle 22 */
-  rectangle22: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    left: '10.46%',
-    right: '16.03%',
-    top: '34.84%',
-    bottom: '43.52%',
-    borderWidth: 3,
-    borderColor: '#D6D6D6',
-    borderRadius: 40,
-  },
+  // /* Rectangle 22 */
+  // rectangle22: {
+  //   boxSizing: 'border-box',
+  //   left: '10.46%',
+  //   right: '16.03%',
+  //   top: '34.84%',
+  //   bottom: '43.52%',
+  //   borderWidth: 3,
+  //   borderColor: '#D6D6D6',
+  //   borderRadius: 40,
+  // },
 
-  rectangle23: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    left: '15.5%',
-    right: '21.99%',
-    top: '38.83%',
-    bottom: '56.8%',
-    borderWidth: 3,
-    borderColor: '#D6D6D6',
-    borderRadius: 40,
-  },
+  // rectangle23: {
+  //   boxSizing: 'border-box',
+  //   left: '15.5%',
+  //   right: '21.99%',
+  //   top: '38.83%',
+  //   bottom: '56.8%',
+  //   borderWidth: 3,
+  //   borderColor: '#D6D6D6',
+  //   borderRadius: 40,
+  // },
 
-  /* Rectangle 25 */
-  rectangle25: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    left: '15.5%',
-    right: '21.99%',
-    top: '48.12%',
-    bottom: '47.5%',
-    borderWidth: 3,
-    borderColor: '#D6D6D6',
-    borderRadius: 40,
-  },
+  // /* Rectangle 25 */
+  // rectangle25: {
+  //   boxSizing: 'border-box',
+  //   left: '15.5%',
+  //   right: '21.99%',
+  //   top: '48.12%',
+  //   bottom: '47.5%',
+  //   borderWidth: 3,
+  //   borderColor: '#D6D6D6',
+  //   borderRadius: 40,
+  // },
 
-  /* Rectangle 24 */
-  rectangle24: {
-    boxSizing: 'border-box',
-    position: 'absolute',
-    left: '15.5%',
-    right: '21.99%',
-    top: '38.83%',
-    bottom: '56.8%',
-    borderWidth: 3,
-    borderColor: '#D6D6D6',
-    borderRadius: 40,
-  },
+  // /* Rectangle 24 */
+  // rectangle24: {
+  //   boxSizing: 'border-box',
+  //   left: '15.5%',
+  //   right: '21.99%',
+  //   top: '38.83%',
+  //   bottom: '56.8%',
+  //   borderWidth: 3,
+  //   borderColor: '#D6D6D6',
+  //   borderRadius: 40,
+  // },
 
-  /* LOGOWANIE */
-  logowanie: {
-    position: 'absolute',
-    left: '31.52%',
-    right: '36.03%',
-    top: '31.09%',
-    bottom: '65.16%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 40,
-    lineHeight: 48,
-    color: '#B3B3B3',
-  },
+  // /* LOGOWANIE */
+  // logowanie: {
+  //   left: '31.52%',
+  //   right: '36.03%',
+  //   top: '31.09%',
+  //   bottom: '65.16%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 40,
+  //   lineHeight: 48,
+  //   color: '#B3B3B3',
+  // },
 
-  /* E-mail */
-  email: {
-    position: 'absolute',
-    left: '18.81%',
-    right: '71.39%',
-    top: '36.56%',
-    bottom: '61.17%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 29,
-    color: '#B3B3B3',
-  },
+  // /* E-mail */
+  // email: {
+  //   left: '18.81%',
+  //   right: '71.39%',
+  //   top: '36.56%',
+  //   bottom: '61.17%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 24,
+  //   lineHeight: 29,
+  //   color: '#B3B3B3',
+  // },
 
-  /* Hasło */
-  haslo: {
-    position: 'absolute',
-    left: '18.81%',
-    right: '72.45%',
-    top: '45.86%',
-    bottom: '51.88%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 29,
-    color: '#B3B3B3',
-  },
+  // /* Hasło */
+  // haslo: {
+  //   left: '18.81%',
+  //   right: '72.45%',
+  //   top: '45.86%',
+  //   bottom: '51.88%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 24,
+  //   lineHeight: 29,
+  //   color: '#B3B3B3',
+  // },
 
-  /* Nie posiadasz konta? */
-  noAccount: {
-    position: 'absolute',
-    left: '15.5%',
-    right: '51.79%',
-    top: '56.48%',
-    bottom: '41.25%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 29,
-    color: '#B3B3B3',
-  },
+  // /* Nie posiadasz konta? */
+  // noAccount: {
+  //   left: '15.5%',
+  //   right: '51.79%',
+  //   top: '56.48%',
+  //   bottom: '41.25%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 24,
+  //   lineHeight: 29,
+  //   color: '#B3B3B3',
+  // },
 
-  /* Nie pamiętam hasła */
-  forgotPassword: {
-    position: 'absolute',
-    left: '45.7%',
-    right: '24.11%',
-    top: '52.5%',
-    bottom: '45.23%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 29,
-    color: '#656565',
-  },
+  // /* Nie pamiętam hasła */
+  // forgotPassword: {
+  //   left: '45.7%',
+  //   right: '24.11%',
+  //   top: '52.5%',
+  //   bottom: '45.23%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 24,
+  //   lineHeight: 29,
+  //   color: '#656565',
+  // },
 
-  /* Zarejestruj się */
-  register: {
-    position: 'absolute',
-    left: '15.5%',
-    right: '62.52%',
-    top: '58.75%',
-    bottom: '38.98%',
-    fontStyle: 'normal',
-    fontWeight: '600',
-    fontSize: 24,
-    lineHeight: 29,
-    color: '#656565',
-  },
+  // /* Zarejestruj się */
+  // register: {
+  //   left: '15.5%',
+  //   right: '62.52%',
+  //   top: '58.75%',
+  //   bottom: '38.98%',
+  //   fontStyle: 'normal',
+  //   fontWeight: '600',
+  //   fontSize: 24,
+  //   lineHeight: 29,
+  //   color: '#656565',
+  // },
 });
