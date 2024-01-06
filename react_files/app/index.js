0a2b9338674b753dc3c6679633cb2ac5b4a60cf9
Milosz180 <milogro@student.agh.edu.pl>
2024-01-06 13:34:39 +0100

    complete front for login in app

--- react_files/app/index.js
+++ react_files/app/index.js
@@ -1,39 +1,57 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
-import { styles_login } from './style-container-top-login';
+import React, {useState} from 'react';
+import { View, Text, Image, ImageBackground, TouchableOpacity, Alert, TextInput } from 'react-native';
+import { styles_login } from './style-login';
+
+
 const Login = () => {
+
+    const [isPressed, setIsPressed] = useState(false);
+
+    const handlePress = () => {
+        setIsPressed(true);
+        setTimeout(() => {
+            setIsPressed(false);
+        }, 500);
+        Alert.alert('Przycisk został naciśnięty!', 'Dodatkowa wiadomość.');
+      };
+
     return (
         <View style={styles_login.container}>
             <View style={styles_login.containerTopBar}>
-            <Image source={require('react_files/app/images/herb_Polski.png')} style={styles_login.herbPolski} />
-            <Text style={styles_login.urzedasPl}>URZĘDAS.PL</Text>
-            <Image source={require('react_files/app/images/info.png')} style={styles_login.info} />
+                <Image source={require('react_files/app/images/herb_Polski.png')} style={styles_login.herbPolski} />
+                <Text style={styles_login.urzedasPl}>URZĘDAS.PL</Text>
+                <TouchableOpacity>
+                    <Image source={require('react_files/app/images/info.png')} style={styles_login.info} />
+                </TouchableOpacity>
             </View>
             <View style={styles_login.lineTop}></View>
+            <Image source={require('react_files/app/images/logo.png')} style={styles_login.logo} />
+            <Text style={styles_login.logowanie}>LOGOWANIE</Text>
+            <View style={styles_login.containerLoginWindow}>
+                <Text style={styles_login.loginText}>E-mail</Text>
+                <TextInput style={styles_login.loginField} placeholder="Wprowadź e-mail" keyboardType="email-address" autoCapitalize="none" placeholderTextColor="#B3B3B3"></TextInput>
+                <Text style={styles_login.loginText}>Hasło</Text>
+                <TextInput style={styles_login.loginField} placeholder="Wprowadź hasło" placeholderTextColor="#B3B3B3" secureTextEntry></TextInput>
+                <TouchableOpacity>
+                    <Text style={styles_login.forgotPassword}>Nie pamiętam hasła</Text>
+                </TouchableOpacity>
+                
+            </View>
+            <ImageBackground source={require('react_files/app/images/orzel_bialy_background.png')} style={styles_login.containerBody}>
+                <Text style={styles_login.noAccount}>Nie posiadasz konta?</Text>
+                <TouchableOpacity>
+                    <Text style={styles_login.register}>Zarejestruj się</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles_login.containerLoginConfirm, isPressed ? styles_login.containerLoginConfirmPressed : null,]} onPress={handlePress}>
+                    <Text style={[styles_login.loginConfirmText, isPressed ? styles_login.loginConfirmTextPressed : null,]}>ZALOGUJ</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+            <View style={styles_login.containerBottomBar}>
+                <Image source={require('react_files/app/images/COI.png')} style={styles_login.coi} />
+                <Image source={require('react_files/app/images/ministerstwo_cyfryzacji.png')} style={styles_login.mc} />
+                <Text style={styles_login.versionText}>wersja 0.1</Text>
+            </View>
         </View>
-
-
-
-                // <View style={styles.rectangle13}></View>
-                // {/* <Image source={require('react_files/app/images/orzel_bialy.png')} style={styles.orzelBialy3} /> */}
-                // <View style={styles.rectangle14}></View>
-                // <View style={styles.rectangle15}></View>
-                // {/* <Image source={require('react_files/app/images/COI.png')} style={styles.coi2} />
-                // <Image source={require('react_files/app/images/ministerstwo_cyfryzacji.png')} style={styles.znakUproszczony2} /> */}
-                // {/* <Text style={styles.wersja01}>wersja 0.1</Text>
-
-                // {/* <Image source={require('react_files/app/images/logo.png')} style={styles.image1} /> */}
-                // <View style={styles.rectangle22}></View>
-                // <View style={styles.rectangle23}></View>
-                // <View style={styles.rectangle25}></View>
-                // <View style={styles.rectangle24}></View>
-                // {/* <Text style={styles.logowanie}>LOGOWANIE</Text>
-                // <Text style={styles.email}>E-mail</Text>
-                // <Text style={styles.haslo}>Hasło</Text>
-                // <Text style={styles.noAccount}>Nie posiadasz konta?</Text>
-                // <Text style={styles.forgotPassword}>Nie pamiętasz hasła?</Text>
-                // <Text style={styles.register}>Zarejestruj się</Text> */}
-    
     );
 };
 export default Login;
--- react_files/app/style-login.js
+++ react_files/app/style-login.js
@@ -1,223 +1,198 @@
+import { StyleSheet } from 'react-native';
+export const styles_login = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: '#FFFFFF',
+  },    
+  containerTopBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#F2F2F2',
+    height: 70,
+  },  
+  herbPolski: {
+    width: 51,
+    height: 60,
+    marginLeft: '16%',
+    marginTop: '1%',
+  },
+  urzedasPl: {
+    marginLeft: '0%',
+    marginTop: '1%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 30,
+    lineHeight: 48,
+    color: '#000000',
+  },
+  info: {
+    marginLeft: '2%',
+    marginTop: '1%',
+    width: 50,
+    height: 50,
+    padding: 0,
+  },
+  lineTop: {
+    borderWidth: 1,
+    borderColor: '#B3B3B3',
+  },
 
+  logo: {
+    marginLeft: '33%',
+    marginTop: '3%',
+    borderWidth: 4,
+    borderColor: '#000000',
+    width: 130,
+    height: 130,
+  },
 
-// //   /* Rectangle 13 */
-// //   rectangle13: {
-// //     left: 0,
-// //     right: '4.64%',
-// //     top: 0,
-// //     bottom: 0,
-// //     backgroundColor: '#FFFFFF',
-// //   },
+  logowanie: {
+    marginLeft: '25%',
+    marginTop: '2%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 34,
+    lineHeight: 34,
+    color: '#B3B3B3',
+  },
 
-//   /* orzel_bialy 3 */
-//   orzelBialy3: {
-//     left: '10.46%',
-//     right: '4.64%',
-//     top: '48.75%',
-//     bottom: '9.61%',
-//     // resizeMode: 'cover', // Dodane dla obrazu
-//     width: '10%' ,
-//     height: '10%' ,
-//   },
+  containerLoginWindow: {
+    marginLeft: '4%',
+    flexDirection: 'flex',
+    borderWidth: 5,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+    height: 174,
+    width: 360,
+  },  
+  
+  loginText: {
+    marginLeft: '10%',
+    marginTop: '1%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#B3B3B3',
+  },  
 
-//   // /* Rectangle 14 */
-//   // rectangle14: {
-//   //   left: 0,
-//   //   right: '4.64%',
-//   //   top: '90.39%',
-//   //   bottom: 0,
-//   //   backgroundColor: '#CB3D49',
-//   // },
+  loginField: {
+    marginLeft: '6%',
+    borderWidth: 3,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+    height: 44,
+    width: 310,
+    paddingLeft: 15,
+  },  
 
-//   // /* Rectangle 15 */
-//   // rectangle15: {
-//   //   // left: 0,
-//   //   // right: '4.64%',
-//   //   // top: 0,
-//   //   // bottom: '92.97%',
-//   //   backgroundColor: '#F2F2F2',
-//   //   height: '3%',
-//   //   marginBottom: '202%',
-//   // },
+  forgotPassword: {
+    marginLeft: '42%',
+    marginTop: '1%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#656565',
+  },  
 
-  /* Line 3 */
+  noAccount: {
+    marginLeft: '14%',
+    marginTop: '1%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#B3B3B3',
+    width: 174,
+  },  
 
+  register: {
+    marginLeft: '14%',
+    marginTop: '1%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#656565',
+    width: 117,
+  },  
 
-//   /* info */
+  containerBody: {
+    height: 246,
+    width: 400,
+  },
 
+  containerBottomBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#CB3D49',
+    height: 62,
+  },  
 
-//   // /* COI 2 */
-//   // coi2: {
-//   //   left: '3.31%',
-//   //   right: '72.45%',
-//   //   top: '92.42%',
-//   //   bottom: '1.95%',
-//   //   // resizeMode: 'cover', // Dodane dla obrazu
-//   //   width: '10%' ,
-//   //   height: '10%' ,
-//   // },
+  coi: {
+    width: 76.75,
+    height: 30,
+    marginLeft: '4%',
+  },
+  
+  mc: {
+    width: 97,
+    height: 43.2,
+    marginLeft: '4%',
+  },
 
-//   // /* 05_znak_uproszczony_kolor_biale_tlo 2 */
-//   // znakUproszczony2: {
-//   //   left: '28.74%',
-//   //   right: '47.15%',
-//   //   top: '92.03%',
-//   //   bottom: '1.64%',
-//   //   // resizeMode: 'cover', // Dodane dla obrazu
-//   // },
+  versionText: {
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#D6D6D6',
+    marginLeft: '25%',
+    marginTop: '6%',
+  },
 
-//   // /* wersja 0.1 */
-//   // wersja01: {
-//   //   left: '73.11%',
-//   //   right: 0,
-//   //   top: '95.94%',
-//   //   bottom: '0.94%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 32,
-//   //   lineHeight: 39,
-//   //   color: '#D6D6D6',
-//   // },
+  containerLoginConfirm: {
+    marginLeft: '31%',
+    marginTop: '5%',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+    height: 56,
+    width: 148,
+    backgroundColor: '#F2F2F2',
+  },  
 
-//   // /* URZĘDAS.PL */
+  containerLoginConfirmPressed: {
+    marginLeft: '31%',
+    marginTop: '5%',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: '#F2F2F2',
+    borderRadius: 40,
+    height: 56,
+    width: 148,
+    backgroundColor: '#D6D6D6',
+  },  
+  
+  loginConfirmText: {
+    fontStyle: 'normal',
+    marginTop: '8%',
+    fontWeight: '600',
+    fontSize: 22,
+    lineHeight: 24,
+    color: '#B3B3B3',
+  }, 
 
+  loginConfirmTextPressed: {
+    fontStyle: 'normal',
+    marginTop: '8%',
+    fontWeight: '600',
+    fontSize: 22,
+    lineHeight: 24,
+    color: '#F2F2F2',
+  }, 
 
-//   /* Herb_Polski 2 */
-
-//   // /* image 1 */
-//   // image1: {
-//   //   left: '31.52%',
-//   //   right: '36.16%',
-//   //   top: '10.47%',
-//   //   bottom: '70.39%',
-//   //   // resizeMode: 'cover', // Dodane dla obrazu
-//   //   borderWidth: 4,
-//   //   borderColor: '#000000',
-//   //   width: '10%' ,
-//   //   height: '10%' ,
-//   // },
-
-//   // /* Rectangle 22 */
-//   // rectangle22: {
-//   //   boxSizing: 'border-box',
-//   //   left: '10.46%',
-//   //   right: '16.03%',
-//   //   top: '34.84%',
-//   //   bottom: '43.52%',
-//   //   borderWidth: 3,
-//   //   borderColor: '#D6D6D6',
-//   //   borderRadius: 40,
-//   // },
-
-//   // rectangle23: {
-//   //   boxSizing: 'border-box',
-//   //   left: '15.5%',
-//   //   right: '21.99%',
-//   //   top: '38.83%',
-//   //   bottom: '56.8%',
-//   //   borderWidth: 3,
-//   //   borderColor: '#D6D6D6',
-//   //   borderRadius: 40,
-//   // },
-
-//   // /* Rectangle 25 */
-//   // rectangle25: {
-//   //   boxSizing: 'border-box',
-//   //   left: '15.5%',
-//   //   right: '21.99%',
-//   //   top: '48.12%',
-//   //   bottom: '47.5%',
-//   //   borderWidth: 3,
-//   //   borderColor: '#D6D6D6',
-//   //   borderRadius: 40,
-//   // },
-
-//   // /* Rectangle 24 */
-//   // rectangle24: {
-//   //   boxSizing: 'border-box',
-//   //   left: '15.5%',
-//   //   right: '21.99%',
-//   //   top: '38.83%',
-//   //   bottom: '56.8%',
-//   //   borderWidth: 3,
-//   //   borderColor: '#D6D6D6',
-//   //   borderRadius: 40,
-//   // },
-
-//   // /* LOGOWANIE */
-//   // logowanie: {
-//   //   left: '31.52%',
-//   //   right: '36.03%',
-//   //   top: '31.09%',
-//   //   bottom: '65.16%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 40,
-//   //   lineHeight: 48,
-//   //   color: '#B3B3B3',
-//   // },
-
-//   // /* E-mail */
-//   // email: {
-//   //   left: '18.81%',
-//   //   right: '71.39%',
-//   //   top: '36.56%',
-//   //   bottom: '61.17%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 24,
-//   //   lineHeight: 29,
-//   //   color: '#B3B3B3',
-//   // },
-
-//   // /* Hasło */
-//   // haslo: {
-//   //   left: '18.81%',
-//   //   right: '72.45%',
-//   //   top: '45.86%',
-//   //   bottom: '51.88%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 24,
-//   //   lineHeight: 29,
-//   //   color: '#B3B3B3',
-//   // },
-
-//   // /* Nie posiadasz konta? */
-//   // noAccount: {
-//   //   left: '15.5%',
-//   //   right: '51.79%',
-//   //   top: '56.48%',
-//   //   bottom: '41.25%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 24,
-//   //   lineHeight: 29,
-//   //   color: '#B3B3B3',
-//   // },
-
-//   // /* Nie pamiętam hasła */
-//   // forgotPassword: {
-//   //   left: '45.7%',
-//   //   right: '24.11%',
-//   //   top: '52.5%',
-//   //   bottom: '45.23%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 24,
-//   //   lineHeight: 29,
-//   //   color: '#656565',
-//   // },
-
-//   // /* Zarejestruj się */
-//   // register: {
-//   //   left: '15.5%',
-//   //   right: '62.52%',
-//   //   top: '58.75%',
-//   //   bottom: '38.98%',
-//   //   fontStyle: 'normal',
-//   //   fontWeight: '600',
-//   //   fontSize: 24,
-//   //   lineHeight: 29,
-//   //   color: '#656565',
-//   // },
+});
