fde0f6a2bfd59b118e9ba02f2acd9bd33a08f391
Wiki <wjanicka@student.agh.edu.pl>
2024-01-08 18:30:45 +0100

    Delete Swagger configuration (no needed)

--- api/src/main/java/com/api/API/ApiApplication.java
+++ api/src/main/java/com/api/API/ApiApplication.java
@@ -3,10 +3,8 @@ package com.api.API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "API", version = "1.0.0"))
 
 public class ApiApplication {
 
