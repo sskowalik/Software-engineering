ca8c3e132a9e5fa31cbbdda17e8cf4fe135f00ca
Milosz180 <milogro@student.agh.edu.pl>
2024-01-21 16:15:56 +0100

    visit planner front 1 version

---
+++ react_files/app/style-visitPlanner.js
@@ -0,0 +1,91 @@
+import { StyleSheet } from 'react-native';
+export const styles_visitPlanner = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: '#FFFFFF',
+  },
+  containerPassRegWindow: {
+    height:1825,
+    backgroundColor: '#FFFFFF',
+    
+  },
+  officesHeader: {
+    marginLeft: '8%',
+    marginTop: '6%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 30,
+    color: '#656575',
+    width: 400,
+  },
+  officesText: {
+    marginLeft: '8%',
+    marginTop: '6%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 20,
+    color: '#656565',
+    textAlign: 'justify',
+    width: 325,
+    
+  },
+  officesFormText:{
+    marginLeft: '15%',
+    marginTop: '6%',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 20,
+    lineHeight: 34,
+    color: '#B3B3B3',
+
+  },
+  officesForm:{
+    marginLeft: '8%',
+    backgroundColor: '#F3F3F3',
+    height: 44,
+    width: 310,
+    paddingLeft: 15,
+  },
+  SubmitButton: {
+    marginLeft: '25%',
+    marginTop: '8%',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: '#D6D6D6',
+    borderRadius: 40,
+    height: 56,
+    width: 200,
+    backgroundColor: '#F2F2F2',
+  },  
+
+  SubmitButtonPressed: {
+    marginLeft: '25%',
+    marginTop: '8%',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: '#CB3D44',
+    borderRadius: 40,
+    height: 56,
+    width: 200,
+    backgroundColor: '#CB3D49',
+  },  
+  
+  SubmitText: {
+    fontStyle: 'normal',
+    marginTop: '7%',
+    fontWeight: '600',
+    fontSize: 22,
+    lineHeight: 24,
+    color: '#B3B3B3',
+  }, 
+
+  SubmitTextPressed: {
+    fontStyle: 'normal',
+    marginTop: '7%',
+    fontWeight: '600',
+    fontSize: 22,
+    lineHeight: 24,
+    color: '#F2F2F2',
+  }, 
+});
--- react_files/app/visitPlanner.js
+++ react_files/app/visitPlanner.js
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import { View, Text, Image, ImageBackground, TouchableOpacity, Alert, TextInput } from 'react-native';
 import { styles_menu } from './style-menu';
+import { styles_visitPlanner } from './style-visitPlanner';
 import { useNavigation } from '@react-navigation/native';
+import { Picker } from '@react-native-picker/picker';
+import provinceData from './provinces';
+import { getDistrictsForProvince, getCommunesForDistrict, handleProvinceChange, handleDistrictChange,} from './provincesFunctions';
 
 const VisitPlanner = () => {
 
@@ -10,6 +14,10 @@ const VisitPlanner = () => {
     const notificationPress = () => {
         navigation.navigate('notifications');
     };
+
+    const [selectedProvince, setSelectedProvince] = useState(null);
+    const [selectedDistrict, setSelectedDistrict] = useState(null);
+    const [selectedCommune, setSelectedCommune] = useState(null);
     
     return (
         <View style={styles_menu.container}>
@@ -22,7 +30,22 @@ const VisitPlanner = () => {
             </View>
             <View style={styles_menu.lineTop}></View>
             <View style={styles_menu.containerBodyTiles}>
-            
+                <Text style={styles_visitPlanner.officesHeader}>Wybierz urząd do którego chcesz się udać!</Text>
+                <Text style={styles_visitPlanner.officesFormText}>Województwo</Text>
+                <Picker selectedValue={selectedProvince} onValueChange={(itemValue) => handleProvinceChange(itemValue, setSelectedProvince, setSelectedDistrict, setSelectedCommune)} style={styles_visitPlanner.officesForm}>
+                    <Picker.Item label="Wybierz województwo" value={null} />{Object.keys(provinceData).map((province, index) => (<Picker.Item key={index} label={province} value={province} />))}
+                </Picker>
+                <Text style={styles_visitPlanner.officesFormText}>Powiat</Text>
+                <Picker selectedValue={selectedDistrict} onValueChange={(itemValue) => handleDistrictChange(itemValue, setSelectedDistrict, setSelectedCommune)} style={styles_visitPlanner.officesForm} enabled={selectedProvince !== null}>
+                     <Picker.Item label="Wybierz powiat" value={null} />{getDistrictsForProvince(selectedProvince, provinceData).map((district, index) => (<Picker.Item key={index} label={district} value={district} />))}
+                </Picker>
+                <Text style={styles_visitPlanner.officesFormText}>Gmina</Text>
+                <Picker selectedValue={selectedCommune} onValueChange={(itemValue) => setSelectedCommune(itemValue)} style={styles_visitPlanner.officesForm} enabled={selectedDistrict !== null}>
+                    <Picker.Item label="Wybierz gminę" value={null} />{getCommunesForDistrict(selectedProvince, selectedDistrict, provinceData).map((commune, index) => (<Picker.Item key={index} label={commune} value={commune} />))}
+                </Picker>
+                <TouchableOpacity style={[styles_visitPlanner.SubmitButton]}>
+                    <Text style={[styles_visitPlanner.SubmitText]}>DALEJ</Text>
+                </TouchableOpacity>
             </View>
             <View style={styles_menu.containerBottomBar}>
                 <Image source={require('react_files/app/images/COI.png')} style={styles_menu.coi} />
