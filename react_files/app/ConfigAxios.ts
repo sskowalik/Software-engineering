7bda579a3b33db2d974d2ae85d3aaa2b2e8ff8a3
sskowalik <skowalik@student.agh.edu.pl>
2024-01-25 23:57:11 +0100

    registration api connected

--- react_files/app/ConfigAxios.ts
+++ react_files/app/ConfigAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:"http://192.168.126.69:8080", //IP za kazdym razem sie zmienia!!!!
+    baseURL:"http://192.168.137.183:8080", //IP za kazdym razem sie zmienia!!!!
     headers:{
         "Content-Type":"application/json"
     }
